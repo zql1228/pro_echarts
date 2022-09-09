5808d368f493767ee36327dded9d51bcf6382f63
zhangqiaoli <2053221503@qq.com>
2022-09-09 18:01:25 +0800

    echarts

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,9 +14,9 @@
     </div>
     <div class="title-right">
       <img src="@/assets/img/right.png" alt=""/>
-      <!-- <img src="@/assets/img/route.png" alt="">
+      <img src="@/assets/img/route.png" alt="">
       <img src="@/assets/img/route2.png" alt="">
-      <img src="@/assets/img/route3.png" alt=""> -->
+      <img src="@/assets/img/route3.png" alt="">
       <span>赣州蓉江新区综合执法大队</span>
     </div>
     </div>
@@ -26,7 +26,7 @@
         <div class="left-t">
           <div class="rectangle-top">
               <h3>线索数量</h3>
-              <div >
+              <div class="rectangle-top-div">
                 <img src="@/assets/img/rectangle6.png" alt="">
                 <img src="@/assets/img/rectangle7.png" alt="">
                 <img src="@/assets/img/route4.png" alt="">
@@ -35,12 +35,12 @@
                   <span>25686条</span>
                 </div>
               </div>
-              <div>
+              <div class="rectangle-top-div">
                 <img src="@/assets/img/rectangle6.png" alt="">
                 <img src="@/assets/img/rectangle7.png" alt="">
-                <img src="@/assets/img/route4.png" alt="">
+                <img src="@/assets/img/vector2.png" alt="">
                 <div>
-                  <div>工程建设领域</div>
+                  <div>非工程建设领域</div>
                   <span>25686条</span>
                 </div>
               </div>
@@ -55,28 +55,100 @@
                 <div style="width:100%;height:100%" id="echarts1"></div>
               </div>
           </div>
-          <div class="rectangle-middle"></div>
-          <div class="rectangle-bottom"></div>
-          
+          <div class="rectangle-middle">
+            <h3>涉及人数</h3>
+              <div class="rectangle-top-div">
+                <img src="@/assets/img/rectangle6.png" alt="">
+                <img src="@/assets/img/rectangle7.png" alt="">
+                <img src="@/assets/img/vector2.png" alt="">
+                <div>
+                  <div>涉及总人数</div>
+                  <span>25686人</span>
+                </div>
+              </div>
+              <div  class="rectangle-top-last">
+                <div>本月新增人数：5221人</div>
+                <div>较上月比：
+                  <img src="@/assets/img/intersect.png" alt="">
+                  2%</div>
+              </div>
+              <div class="rectangle-middle-echarts">
+                <div style="width:100%;height:100%" id="echarts2"></div>
+              </div>
+          </div>
+          <div class="rectangle-bottom">
+            <h3>涉及金额</h3>
+              <div class="rectangle-top-div">
+                <img src="@/assets/img/rectangle6.png" alt="">
+                <img src="@/assets/img/rectangle7.png" alt="">
+                <img src="@/assets/img/route5.png" alt="">
+                <div>
+                  <div>涉及总金额</div>
+                  <span>25686人</span>
+                </div>
+              </div>
+              <div class="rectangle-top-div">
+                <img src="@/assets/img/rectangle6.png" alt="">
+                <img src="@/assets/img/rectangle7.png" alt="">
+                <img src="@/assets/img/route4.png" alt="">
+                <div>
+                  <div>工程建设领域</div>
+                  <span>25686条</span>
+                </div>
+              </div>
+              <div class="rectangle-top-div">
+                <img src="@/assets/img/rectangle6.png" alt="">
+                <img src="@/assets/img/rectangle7.png" alt="">
+                <img src="@/assets/img/vector2.png" alt="">
+                <div>
+                  <div>非工程建设领域</div>
+                  <span>25686条</span>
+                </div>
+              </div>
+              <div class="rectangle-bottom-echarts">
+                <div style="width:100%;height:100%" id="echarts3"></div>
+              </div>
+          </div>
         </div>
       </div>
       <div  class="content-right">
+        <div  class="content-right-top">
+          <div class="content-right-top-div">
+            <div>案件办结率</div>
+            <div>95<span>%</span></div>
+            <div>
+              <img src="" alt="">
+            </div>
+          </div>
+         
+        </div>
+        <div  class="content-right-bottom-top">
+          <div>线索上报趋势</div>
+        </div>
+        <div  class="content-right-bottom">
+          <div id="echarts4" style="width:100%;height:100%"></div>
+        </div>
       </div>
+
+    </div>
+    <div id="map">
 
     </div>
   </div>
 </template>
 
 <script>
+import AMapLoader from "@amap/amap-jsapi-loader";
 import moment from 'moment'
-import {chart1} from '@/views/echarts/echarts'
+import {chart1,chart2,chart3,chart4} from '@/views/echarts/echarts'
 export default {
   name: 'HomeView',
   components: {
   },
   data:()=>{
     return {
-      nowTime:''
+      nowTime:'',
+      map:null,
     }
   },
   mounted(){
@@ -86,13 +158,34 @@ export default {
            this.getNowTime(); //每秒更新一次时间
         }, 1000); 
         this.$nextTick(()=>{
-          chart1() 
+          chart1()
+          chart2() 
+          chart3()
+          chart4()
         })  
+      this.initMap()
        
   },
   methods:{
     getNowTime(){
       this.nowTime =moment(new Date()).format('YYYY.MM.DD hh:mm:ss') 
+    },
+    initMap() {
+      const that = this;
+      AMapLoader.load({
+        key: "8ede6988a3a58a39d1b605a9d0790642", // 申请好的Web端开发者Key，首次调用 load 时必填
+        version: "2.0", // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
+        plugins: ["AMap"], // 需要使用的的插件列表，如比例尺'AMap.Scale'等
+      }).then((AMap) => {
+        that.map = new AMap.Map("map", {
+          //设置地图容器id
+          // viewMode: "3D", //是否为3D地图模式
+          resizeEnable: true,
+          zoom: 11, //初始化地图级别
+          center: [114.933494, 25.831139], //初始化地图中心点位置
+          mapStyle:"amap://styles/darkblue", //显示样式
+        });
+      })
     }
   }
 }
@@ -101,9 +194,8 @@ export default {
 @primary-color: #0E9CFF;
 .container{
   height: 100vh;
-  width: 100%;
+  width: 100vw;
   position: relative;
-  background: linear-gradient(270deg, #3C679E 3%, rgba(60,103,158,0.00) 100%);
   background-attachment: fixed;
   mix-blend-mode:multiply;/*混合模式：穿透*/
 }
@@ -130,6 +222,7 @@ export default {
   span{
     left:50px;
     top:10px;
+    color:#fff;
   }
 
 }
@@ -153,9 +246,28 @@ export default {
 }
 .title-right{
   position: relative;
-  width:658px;
+  width:663px;
   img,span{position: absolute;}
   img:nth-child(1){top:-2px;left:-37px;z-index: -1;width:663px}
+  img:nth-child(2){
+    right:210px;
+    top:12px;
+    width:19px;
+    height:19px;
+  }
+  img:nth-child(3){
+    top:21px;
+    right:212px;
+  }
+  img:nth-child(4){
+    top:15px;
+    right:216px;
+  }
+  span{
+    color:#fff;
+    top:10px;
+    right:10px;
+  }
 }
 }
 .content{
@@ -189,7 +301,7 @@ export default {
         box-sizing: border-box;
         padding:18px 17px
       }
-      .rectangle-top{
+      .rectangle-top,.rectangle-middle,.rectangle-bottom{
         width: 379.73px;
         height: 213.24px;
         background:url('@/assets/img/rectangle3.png') no-repeat;
@@ -202,7 +314,7 @@ export default {
         &>div:nth-child(3){
           top:50px;
         }
-        &>div:nth-child(2),&>div:nth-child(3){
+        .rectangle-top-div{
           position: relative;
           &>img{
             position: absolute;
@@ -218,7 +330,7 @@ export default {
           &>div:last-child{
             position: absolute;
             color:#fff;
-            font-size: 14px;
+            font-size: 13px;
             left:50px;
             span{
               color:@primary-color;
@@ -255,18 +367,87 @@ export default {
         height: 392.6px;
         background:url('@/assets/img/rectangle4.png') no-repeat; 
         background-size: 100% 100%;
+        .rectangle-middle-echarts{
+          width:379px;
+          height:300px;
+        }
       }
       .rectangle-bottom{
         width: 379.73px;
         height: 213.24px;
+        position: relative;
         background:url('@/assets/img/rectangle5.png') no-repeat;
         background-size: 100% 100%;
+        div.rectangle-top-div:nth-child(4){
+          top:100px;
+        }
+        .rectangle-bottom-echarts{
+          position: absolute;
+          width:230px;
+          height:200px;
+          right:0px;
+          top:0px;
+        }
       }
     }
   }
   .content-right{
- 
+    .content-right-top{
+      position: absolute;
+      left:480px;
+      top:110px;
+      .content-right-top-div{
+        width: 210px;
+        height: 110px;
+        padding:21px 17px;
+        background: url('@/assets/img/rectangle10.png') no-repeat;
+        background-size: 100% 100%;
+        box-sizing: border-box;
+        div:nth-child(1){
+          color:#A8D6FF;
+          font-size: 14px;
+          margin-bottom:10px;
+        }
+        div:nth-child(2){
+          color:@primary-color;
+          font-size: 36px;
+          span{font-size: 18px;padding-left:20px;}
+        }
+
+      }
+    }
+    .content-right-bottom-top{
+      position: absolute;
+      width: 189px;
+      height: 51px; 
+      left:445px;
+      bottom:120px;
+      background: url('@/assets/img/rectangle.png') no-repeat;
+      background-size: cover;
+      text-align: center;
+      line-height: 51px;
+      color:@primary-color;    
+    }
+    .content-right-bottom{
+      position: absolute;
+      right: 17px;
+      bottom: -90px;
+      height: 212.81px;
+      width: 1459px;
+      background: url('@/assets/img/rectangle9.png') no-repeat;
+      background-size: 100% 100%;
+    }
   }
 }
-
+//地图
+#map{
+  position: absolute;
+  width:100%;
+  height:100%;
+  top:0;
+  left:0;
+  bottom:0;
+  right:0;
+  z-index:-10
+}
 </style>
--- src/views/echarts/echarts.js
+++ src/views/echarts/echarts.js
@@ -36,20 +36,21 @@ const option = {
   },
   series: [
     {
-      name: arrx[1].value,
+      name: '工程领域',
       type: 'pie',
       radius: ['64%', '85%'],
       label: {
             show: true,
             position: 'center', // 数值显示在内部
-            formatter:'{c}\n{a}',
-            fontSize:24,
+            formatter:[`{a|${arrx[0].value}}`,`{x|${arrx[1].value}}`].join('\n'),
             rich: {
                 a: {
-                    color: '#1890ff'
+                    color: '#1890ff',
+                    fontSize:26
                 },
-                c: {
-                    color:'#F4A305'
+                x: {
+                    color:'#F4A305',
+                    fontSize:26
                 }
             },
         },
@@ -66,4 +67,242 @@ let chartDom = document.getElementById('echarts1');
     return myChart1
     }
 }
+export const chart2=()=>{
+const option = {
+  legend: {
+    data: ['新增人数'],
+    top:'10%',
+    right:'20%',
+    textStyle:{
+      color:'#0e9cff'
+    },
+    itemStyle:{
+      opacity:0
+    }
+  },
+  toolbox: {
+    feature: {//自定义工具按钮
+        myTool1: {
+            show: true,
+            title: '2022年',
+            icon: 'path://./image/union.png',
+            onclick: function (){
+                alert('myToolHandler1')
+            }
+        },
+        brush:{
+          type:'lineY',
+          title:'2022年'
+        }
+    }
+},
+  xAxis: {
+    type: 'category',
+    boundaryGap: false,
+    axisLine:{
+      symbol:'arrow',
+      symbolOffset:[-15,15],
+      symbolSize:[6,10],
+      lineStyle:{
+        color:'#019cff'
+      }
+    },
+    data: ['6月', '7月', '8月', '9月', '10月']
+  },
+  yAxis: {
+    type: 'value',
+    name:'单位：人',
+    splitLine:{show:false},
+    axisTick:{
+      show:true,
+      lineStyle:{
+        color:'#019cff',
+      }
+    },
+    data:[20,50,100,400,800,1000]
+  },
+  series: [
+    {
+      data: [820, 932, 901, 934, 200, 300,500],
+      type: 'line',
+      smooth:true,
+      name:'新增人数',
+      areaStyle: {
+        color:{
+          type: 'linear',
+          x: 0,
+          y: 0,
+          x2: 0,
+          y2: 1,
+          colorStops: [{
+              offset: 0, color: '#0e9cff' // 0% 处的颜色
+          }, {
+              offset: 1, color:'rgba(91,233,255,0.00)' // 100% 处的颜色
+          }],
+          global: false // 缺省为 false
+        }
+      }
+    }
+  ]
+};
+let chartDom = document.getElementById('echarts2');
+    if(chartDom){
+    const myChart2 = echarts.init(chartDom);
+    myChart2.setOption(option)
+    }
 
+}
+export  const chart3=()=>{
+  let arrx= [
+      { value:1000, name: '工程领域' },
+      { value: 2642, name: '非工程领域' },
+    ]
+  arrx[0].label={
+    padding:[15,-160,15,-45]
+  }
+  const option = {
+    tooltip: {
+      trigger: 'item',
+    },
+    legend: {
+      itemStyle:{opacity:0},
+      textStyle:{opacity:0}
+    },
+    series: [
+      {
+        name: '领域',
+        type: 'pie',
+        roseType:'radius',
+        radius: ['30%', '65%'],
+        label: {
+              show: true, // 数值显示在内部
+              formatter: function(param){
+                return '{b|'+param.data.name+'\n}'+'{d|'+param.percent.toFixed(1)+'% }'
+              },
+              padding:[15,-60,15,-80],
+              rich: {
+                b: {
+                  fontSize:12,
+                  color:'#fff',
+                  lineHeight:30,
+                },
+                d: {
+                  fontSize:16,
+                  color:'#0e9cff',
+                  fontWeight:'bold',             
+                }
+              },
+          }, 
+          labelLine: {
+            show: true,//数据标签引导线
+            length:20,
+            length2: 40,
+            lineStyle: {
+                width: 1,
+                type: 'solid'
+            }
+        },    
+        color:[{
+          type: 'radial',
+          x: 0.5,
+          y: 0.5,
+          r: 0.5,
+          colorStops: [{
+              offset: 0, color: 'rgba(71,255,255,0.00)' // 0% 处的颜色
+          }, {
+              offset: 1, color: '#0e9cff' // 100% 处的颜色
+          }],
+          global: false // 缺省为 false
+        },
+        {
+          type: 'radial',
+          x: 0.5,
+          y: 0.5,
+          r: 0.5,
+          colorStops: [{
+              offset: 0, color: 'rgba(75,165,255,0.00)' // 0% 处的颜色
+          }, {
+              offset: 1, color: '#3ccff9' // 100% 处的颜色
+          }],
+          global: false // 缺省为 false
+        }
+      ],
+        data:arrx
+      }
+    ]
+  };
+  let chartDom = document.getElementById('echarts3');
+      if(chartDom){
+      const myChart3 = echarts.init(chartDom);
+      myChart3.setOption(option)
+      }
+  }
+export const chart4=()=>{
+ const option = {
+    legend: {
+      top:'2%',
+      right:'5%',
+      itemStyle:{
+        opacity:0
+      },
+      data: [{
+        name:'涉及金额',
+        textStyle:{ color:'#23dc79',},
+    },{name:'涉及人数', textStyle:{ color:'#0090f7',}}, {name:'欠薪数量', textStyle:{  color:'#5be9ff'}}]
+    },
+    grid: {
+      left: '1%',
+      right: '1%',
+      bottom: '5%',
+      containLabel: true
+    },
+    xAxis: {
+      type: 'category',
+      boundaryGap: false,
+      data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月','8月', '9月', '10月', '11月', '12月']
+    },
+    yAxis: {
+      type: 'value',
+      axisLine:{
+        lineStyle:{
+          color:'rgba(91, 233, 255, 0.1)'
+        }
+      },
+      data:[0,2000,4000,6000,8000,10000]
+    },
+    series: [
+      {
+        name: '涉及金额',
+        type: 'line',
+        smooth: true,
+        lineStyle:{
+          color:'#23dc79',
+        },
+        data: [9200, 1320, 7010, 1340, 9000, 2300, 2100,3300,8800,10000,6600,9900]
+      },
+      {
+        name: '涉及人数',
+        type: 'line',
+        smooth: true,
+        lineStyle:{
+          color:'#0090f7',
+        },
+        data: [6200, 5820, 1910, 2340, 2900, 3300, 3100,1110,4440,9900,1000,8900]
+      },
+      {
+        name: '欠薪数量',
+        type: 'line',
+        smooth: true,
+        lineStyle:{
+          color:'#5be9ff'
+        },
+        data: [3500, 2320, 6010, 1540, 1900, 3300, 4100,1000,4500,3900,6600,2000]
+      },
+    ]
+  };
+  let chartDom = document.getElementById('echarts4');
+  if(chartDom){
+  const myChart4 = echarts.init(chartDom);
+  myChart4.setOption(option)
+  } 
+}
